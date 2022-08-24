69339176908b82d32f31f28878fcced37997b69f
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-08-24 09:27:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,8 @@ using namespace std;
 
 int main(int argc, char **argv) {
     cout << "Coding language\n";
-    string input = argv;
-    cout << input << endl;
-//     fstream infile(input);
-//     string temptext;
-//     string code;
-//     while (getline(infile, temptext)){
-//         code += temptext;
-//     }
-    
+    if (argc > 1) {
+        std::string input(argv[1]);
+    }
+    cout << input << endl; 
 }
