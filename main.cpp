d03a46ec1e3e949ab35b7d788cb3ac35c7e6f0c9
Thetoyking91 <59570607+Thetoyking91@users.noreply.github.com>
2022-08-25 16:37:43 +0000

    everything

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+int split(char str[]) {
+    char *ptr; // declare a ptr pointer  
+    ptr = strtok(str, ":, "); // use strtok() function to separate string using comma (,) delimiter.  
+    cout << " \n Split string using strtok() function: " << endl;  
+    // use while loop to check ptr is not null  
+    while (ptr != NULL)  
+    {  
+        cout << ptr  << endl; // print the string token  
+        ptr = strtok (NULL, ":, ");  
+    }  
+  return 0;
+}
+
 int main(int argc, char **argv) {
   if (argc > 1) {
     cout << "Coding language\n";
@@ -11,18 +24,12 @@ int main(int argc, char **argv) {
     std::string input(argv[1]);
     
     cout << input << endl;
+    fstream inputfile(input);
+    file << inputfile;
+    inputfile.close();
+    int len = file.length();
+    char str[len + 1];
+    strcpy(str, file.c_str());
+    return split(str);
   }
-}
-
-int split(str) {
-    char *ptr; // declare a ptr pointer  
-    ptr = strtok(str, " , "); // use strtok() function to separate string using comma (,) delimiter.  
-    cout << " \n Split string using strtok() function: " << endl;  
-    // use while loop to check ptr is not null  
-    while (ptr != NULL)  
-    {  
-        cout << ptr  << endl; // print the string token  
-        ptr = strtok (NULL, " , ");  
-    }  
-
 }
